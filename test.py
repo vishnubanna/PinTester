01beb61578a739248d201119766917f593e8e923
vishnubanna <banna3vishnu@gmail.com>
2019-02-05 15:15:32 -0500

    test

--- test.py
+++ test.py
@@ -1,19 +1,32 @@
-import wiringpi2 as wp
+import wiringpi2 as wiringpi
 from time import sleep
 
-wp.wiringPiSetupGpio()
-wp.pinMode(17, 1)
+wiringpi.wiringPiSetupGpio()
+wiringpi.pinMode(17, 1)
+wiringpi.pinMode(27, 1)
 input = raw_input("run")
 if input == "run":
   run = True
+  wiringpi.digitalWrite(17, 0)
+  wiringpi.digitalWrite(27, 0)
   while (run):
     try:
-      wp.digitalwrite(17, 1)
-      sleep(2)
-      wp.digitalwrite(17, 0)
+      wiringpi.digitalWrite(17, 1)
+      sleep(1)
+      wiringpi.digitalWrite(27, 1)
+      print("HIGH")
+      sleep(3)
+      wiringpi.digitalWrite(17, 0)
+      sleep(1)
+      wiringpi.digitalWrite(27, 0)
+      print("LOW")
+      sleep(3)
     except KeyboardInterrupt:
       run = False
-
+      wiringpi.digitalWrite(17, 0)
+      wiringpi.digitalWrite(27, 0)
+      sleep(1)
+      print("end")
 
 
 
