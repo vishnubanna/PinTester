2bf0bd13311ddbc89dcef74445bff8a1a555ba8f
vishnubanna <banna3vishnu@gmail.com>
2019-02-17 23:12:28 -0500

    main section for raising and lowering individual pins

--- manual_mode.py
+++ manual_mode.py
@@ -1,9 +1,11 @@
 import time
 import threading
-import wiringpi2 as wp
+#import wiringpi2 as wp
+import wp
 from Que import Que
 
 TIME_INIT = time.clock()
+count = 0
 q = Que()
 pin_lock = threading.Lock()
 solonoid = [17,27,22,5,6]
@@ -30,13 +32,13 @@ wp.pinMode(resis[4], 1)
 def manual():
     try:
         global rised
-        count = 0
+        global count
         pin = raw_input("which pin raised or lowered?")
-        if pin == 'a':
-            print("exiting")
-            return False
-        pin = int(pin)
-        if (pin >= 1 and pin <= 5):
+        try:
+            pin = int(pin)
+        except:
+            pin = raw_input("which pin raised or lowered?")
+        if (count <= 5) and (pin >= 1 and pin <= 5):
             if rised[pin-1] == pin:
                 count -= 1
                 lower(pin-1, pin-1)
@@ -46,17 +48,19 @@ def manual():
                 return "lowered"
             elif rised[pin-1] == 0:
                 rise(pin-1, pin-1)
-                q.put(pin)
                 count += 1
                 rised[pin-1] = pin
                 print(rised)
                 print("pin {} has been raised".format(pin))
                 return pin
-
-        else:
+        elif count > 5:
             pass
-    except KeyboardInterrupt:
-        print("manual off")
+        elif pin > 5:
+            print("not a pin")
+            pass
+        elif pin == 0:
+            return False
+    except:
         return False
 
 def rise(sol, res):
@@ -81,25 +85,25 @@ def lower(sol, res):
     wp.digitalWrite(resis[res], 1)
     return
 
-def timer(tim,pin):
+
+def timer(tim, pin, length):
     global q
     global rise
     on = True
-    while on:
-        global solonoid, resis
-        if (time.clock() - tim) > 5:
-            #with pin_lock:
-                wp.digitalWrite(solonoid[pin - 1], 0)
-                time.sleep(0.00002)
-                wp.digitalWrite(resis[pin - 1], 1)
-                print ("\n pin {} is off".format(pin))
-                print (time.clock() - tim)
-                on = False 
-                #rise[pin-1] = 0
+    time.sleep(length)
+    if (time.clock() - tim) > length:
+            if rised[pin-1] == pin:
+                with pin_lock:
+                    lower(pin-1, pin-1)
+                print(time.clock() - tim)
+                print("time force pin {} lowered".format(pin))
+
+
 
 def main():
     on = True
     print("maunual mode - initial start up test")
+    global rised
     if on:
         try:
             test = []
@@ -108,24 +112,28 @@ def main():
             q.put(3)
             q.put(4)
             q.put(5)
+            q.lister()
+            time.sleep(1)
             while q.has_values():
                 q.lister()
                 pin = q.get()
                 rise(pin - 1, pin - 1)
-                t1 = threading.Thread(target= timer, args=(time.clock(), pin))
+                t1 = threading.Thread(target= timer, args=(time.clock(), pin, 1))
                 t1.daemon = True
                 t1.start()
                 test.append(t1)
             for thread in test:
                 thread.join()
+            rised = [0]*len(solonoid)
         except:
             print("error")
     try:
         while on:
             valid = manual()
-            if valid != "lowered":
+            if type(valid) == int:
                 q.put(valid)
-                t = threading.Thread(target= timer, args=(TIME_INIT, q.get()))
+                q.lister()
+                t = threading.Thread(target= timer, args=(TIME_INIT, q.get(), 5))
                 t.daemon = True
                 t.start()
                 threads.append(t)
@@ -142,4 +150,7 @@ def main():
                 thread.join()
     except KeyboardInterrupt:
         on = False
-        return
+
+
+if __name__ == "__main__":
+    main()
