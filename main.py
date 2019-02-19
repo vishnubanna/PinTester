91cca54fb76834159bae1d72d02078d18d82e818
vishnubanna <banna3vishnu@gmail.com>
2019-02-19 14:22:52 -0500

    main section for raising and lowering individual pins

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import test
 from os import popen
-import manual_mode
+import manual_mode2
 
 choice = raw_input("would you like to use automatic or manual mode?")
 while True:
@@ -8,7 +8,7 @@ while True:
         if choice == "a":
             test.automatic()
         elif choice == "m":
-            manual_mode.main()
+            manual_mode2.main()
         else:
             choice = raw_input("would you like to use automatic or manual mode?")
     except KeyboardInterrupt:
