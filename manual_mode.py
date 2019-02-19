fa9970d3a6c5092aa8b0f04076b74e6a44b692b7
vishnubanna <banna3vishnu@gmail.com>
2019-02-19 13:28:09 -0500

    test

--- manual_mode.py
+++ manual_mode.py
@@ -1,9 +1,11 @@
+
 import time
 import threading
 import wiringpi2 as wp
 from Que import Que
 
 TIME_INIT = time.clock()
+c = 0
 q = Que()
 pin_lock = threading.Lock()
 solonoid = [17,27,22,5,6]
@@ -30,15 +32,15 @@ wp.pinMode(resis[4], 1)
 def manual():
     try:
         global rised
-        count = 0
+        global c
         pin = raw_input("which pin raised or lowered?")
         if pin == 'a':
             print("exiting")
             return False
         pin = int(pin)
-        if (pin >= 1 and pin <= 5):
+        if (c <= 5) and (pin >= 1 and pin <= 5):
             if rised[pin-1] == pin:
-                count -= 1
+                c -= 1
                 lower(pin-1, pin-1)
                 rised[pin-1] = 0
                 print(rised)
@@ -47,7 +49,7 @@ def manual():
             elif rised[pin-1] == 0:
                 rise(pin-1, pin-1)
                 q.put(pin)
-                count += 1
+                c += 1
                 rised[pin-1] = pin
                 print(rised)
                 print("pin {} has been raised".format(pin))
@@ -81,13 +83,13 @@ def lower(sol, res):
     wp.digitalWrite(resis[res], 1)
     return
 
-def timer(tim,pin):
+def timer(tim,pin,length):
     global q
     global rise
     on = True
     while on:
         global solonoid, resis
-        if (time.clock() - tim) > 5:
+        if (time.clock() - tim) > length:
             #with pin_lock:
             wp.digitalWrite(solonoid[pin - 1], 0)
             time.sleep(0.00002)
@@ -114,7 +116,7 @@ def main():
                 q.lister()
                 pin = q.get()
                 rise(pin - 1, pin - 1)
-                t1 = threading.Thread(target= timer, args=(time.clock(), pin))
+                t1 = threading.Thread(target= timer, args=(time.clock(), pin, 1))
                 t1.daemon = True
                 t1.start()
                 test.append(t1)
@@ -127,7 +129,7 @@ def main():
             valid = manual()
             if valid != "lowered":
                 q.put(valid)
-                t = threading.Thread(target= timer, args=(TIME_INIT, q.get()))
+                t = threading.Thread(target= timer, args=(time.clock(), q.get(), 5))
                 t.daemon = True
                 t.start()
                 threads.append(t)
