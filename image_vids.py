4331b34eedb0948957d6f3f57471f8d29096c2fc
Vishnu Banna <vishnubanna@pal-nat186-65-31.itap.purdue.edu>
2019-03-19 13:52:04 -0400

    test

--- image_vids.py
+++ image_vids.py
@@ -1,42 +1,42 @@
 import cv2
 import numpy as np
-import math
-import matplotlib as plt
 import time
+import pycamera.array
+from picamera import PiCamera
 
+camera = PiCamera()
+camera.resolution = (640,480)
+camera.imgrate = 20
+rawCapture = PiRGBArray(camera, size = (640,480))
+
+time.sleep(0.1)
 #vid = cv2.VideoCapture('testp.mp4')
 cap = cv2.VideoCapture(0)
 #fourcc = cv2.VideoWriter_fourcc(*'XVID')
 #out = cv2.VideoWriter('output.avi', fourcc, 20.0, (640,480))
-past_frame = []
-while True:
+past_img = []
+#while True:
+for frame in camera.capture_continuous(rawCapture, format = "bgr", use_video_port = True)
     #time.sleep(0.3)
-    ret, frame = cap.read()
-    cv2.imshow('frame', frame)
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    #cv2.circle(gray, (100,63), 100, (255, 255,255), -1)
+    #ret, img = cap.read()
+
+    img = frame.array
+
+    cv2.imshow('img', img)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     try:
-        delta = cv2.subtract(past_frame, gray)
+        delta = cv2.subtract(past_img, gray)
     except:
         delta = gray
-    past_frame = gray
+    past_img = gray
     cv2.imshow('gray', delta)
-    #out.write(frame)
+    #out.write(img)
     art = np.asarray(delta)
     total = 0
-    #xrange = len(art)
-    #yrange = len(art[1])
-    #vol = xrange*yrange
-    #for i in range(0, xrange):
-        #for j in range(0, yrange):
-            #total += art[i][j]
-    #avg = (total/vol)
     avg = np.average(art)
-    if (avg > 0.5):
+    rawCapture.truncate(0)
+    if (avg > 1):
         print(avg, "motion detected")
-    #else:
-        #print(avg, "nothing")
-
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
