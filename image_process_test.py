9272fe5083ffb76a31883c45bd0629ceed46fb57
vishnubanna <banna3vishnu@gmail.com>
2019-04-07 17:05:20 -0400

    test

--- image_process_test.py
+++ image_process_test.py
@@ -10,7 +10,7 @@ camera.resolution = (res[0], res[1])
 camera.framerate = 15
 rawCapture = PiRGBArray(camera, size = (res[0], res[1]))
 startCase = np.empty((res[0],res[1],3), dtype = np.uint32)
-endCase = np.empty((res[0],res[1],1), dtype = np.uint32)
+endCase = np.empty((res[0],res[1],3), dtype = np.uint32)
 delta = 0
 
 last_frame = []
@@ -18,7 +18,7 @@ sleep(0.1)
 
 for frame in camera.capture_continuous(rawCapture, format = 'bgr', use_video_port = True):
     image = frame.array
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    #image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
     try:
         delta = cv2.subtract(endCase, image)
