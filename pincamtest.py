130d88724750b062bab850ba771fd5a4e09e3ee6
vishnubanna <banna3vishnu@gmail.com>
2019-04-14 15:30:54 -0400

    t

--- pincamtest.py
+++ pincamtest.py
@@ -163,7 +163,7 @@ with PiCamera() as camera:
             sleep(1)
 
             print("setfail: {}".format(rando))
-            print("setfail: {}".format(rand01))
+            print("setfail: {}".format(rando1))
 
             with rawCapture1 as output1:
                 camera.capture(output1, 'bgr')
