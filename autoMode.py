d51c5398e45638d2a1714f10c2c864cbaf84c693
vishnubanna <banna3vishnu@gmail.com>
2019-04-23 14:16:09 -0400

    test

--- autoMode.py
+++ autoMode.py
@@ -9,98 +9,98 @@ import pycamera.array
 from picamera import PiCamera
 
 def automatic():
-  wiringpi.wiringPiSetupGpio()
-  wiringpi.pinMode(17, 1)
-  wiringpi.pinMode(27, 1)
-  wiringpi.pinMode(22, 1)
-  wiringpi.pinMode(5, 1)
-  wiringpi.pinMode(6, 1)
+    wiringpi.wiringPiSetupGpio()
+    wiringpi.pinMode(17, 1)
+    wiringpi.pinMode(27, 1)
+    wiringpi.pinMode(22, 1)
+    wiringpi.pinMode(5, 1)
+    wiringpi.pinMode(6, 1)
 
-  timeLen = 0.004
+    timeLen = 0.004
 
-  input = raw_input("run y/n")
+    input = raw_input("run y/n")
 
-  if input == "y":
-    run = 0
-    wiringpi.digitalWrite(17, 0)
-    wiringpi.digitalWrite(27, 0)
-    wiringpi.digitalWrite(22, 0)
-    wiringpi.digitalWrite(5, 0)
-    wiringpi.digitalWrite(6, 0)
-    print("running 10000 cycles on all pins")
-    
+    if input == "y":
+        run = 0
+        wiringpi.digitalWrite(17, 0)
+        wiringpi.digitalWrite(27, 0)
+        wiringpi.digitalWrite(22, 0)
+        wiringpi.digitalWrite(5, 0)
+        wiringpi.digitalWrite(6, 0)
+        print("running 10000 cycles on all pins")
 
-    camera = PiCamera()
-    
-    camera.resolution = (640,480)
-    camera.framerate = 20
 
-    camera.start_recording('test_no_fail_detection.h264')
-    camera.start_preview()
-    
-    cycles = 10000
+        camera = PiCamera()
 
-    while (run < cycles):
-        try:
-            wiringpi.digitalWrite(17, 1)
-            sleep(timeLen)
-            wiringpi.digitalWrite(27, 1)
-            sleep(timeLen)
-            wiringpi.digitalWrite(22, 1)
-            sleep(timeLen)
-            wiringpi.digitalWrite(5, 1)
-            sleep(timeLen)
-            wiringpi.digitalWrite(6, 1)
-            print("HIGH")
-            sleep(timeLen)
-            wiringpi.digitalWrite(17, 0)
-            sleep(timeLen)
-            wiringpi.digitalWrite(27, 0)
-            sleep(timeLen)
-            wiringpi.digitalWrite(22, 0)
-            sleep(timeLen)
-            wiringpi.digitalWrite(5, 0)
-            sleep(timeLen)
-            wiringpi.digitalWrite(6, 0)
-            print("LOW")
-            sleep(2)
-            wiringpi.digitalWrite(17, 1)
-            sleep(timeLen)
-            wiringpi.digitalWrite(27, 1)
-            sleep(timeLen)
-            wiringpi.digitalWrite(22, 1)
-            sleep(timeLen)
-            wiringpi.digitalWrite(5, 1)
-            sleep(timeLen)
-            wiringpi.digitalWrite(6, 1)
-            print("HIGH")
-            sleep(timeLen)
-            wiringpi.digitalWrite(17, 0)
-            sleep(timeLen)
-            wiringpi.digitalWrite(27, 0)
-            sleep(timeLen)
-            wiringpi.digitalWrite(22, 0)
-            sleep(timeLen)
-            wiringpi.digitalWrite(5, 0)
-            sleep(timeLen)
-            wiringpi.digitalWrite(6, 0)
-            print("LOW")
-            sleep(timeLen)
-        except KeyboardInterrupt:
-            run = cycles
-            camera.stop_recording()
-            camera.stop_preview()
-            wiringpi.digitalWrite(17, 0)
-            wiringpi.digitalWrite(27, 0)
-            wiringpi.digitalWrite(22, 0)
-            wiringpi.digitalWrite(5, 0)
-            wiringpi.digitalWrite(6, 0)
-            sleep(1)
-            print("User ended premature")
+        camera.resolution = (640,480)
+        camera.framerate = 20
 
-    camera.stop_recording()
-    camera.stop_preview()
-    return
+        camera.start_recording('test_no_fail_detection.h264')
+        camera.start_preview()
+
+        cycles = 10000
+
+        while (run < cycles):
+            try:
+                wiringpi.digitalWrite(17, 1)
+                sleep(timeLen)
+                wiringpi.digitalWrite(27, 1)
+                sleep(timeLen)
+                wiringpi.digitalWrite(22, 1)
+                sleep(timeLen)
+                wiringpi.digitalWrite(5, 1)
+                sleep(timeLen)
+                wiringpi.digitalWrite(6, 1)
+                print("HIGH")
+                sleep(timeLen)
+                wiringpi.digitalWrite(17, 0)
+                sleep(timeLen)
+                wiringpi.digitalWrite(27, 0)
+                sleep(timeLen)
+                wiringpi.digitalWrite(22, 0)
+                sleep(timeLen)
+                wiringpi.digitalWrite(5, 0)
+                sleep(timeLen)
+                wiringpi.digitalWrite(6, 0)
+                print("LOW")
+                sleep(2)
+                wiringpi.digitalWrite(17, 1)
+                sleep(timeLen)
+                wiringpi.digitalWrite(27, 1)
+                sleep(timeLen)
+                wiringpi.digitalWrite(22, 1)
+                sleep(timeLen)
+                wiringpi.digitalWrite(5, 1)
+                sleep(timeLen)
+                wiringpi.digitalWrite(6, 1)
+                print("HIGH")
+                sleep(timeLen)
+                wiringpi.digitalWrite(17, 0)
+                sleep(timeLen)
+                wiringpi.digitalWrite(27, 0)
+                sleep(timeLen)
+                wiringpi.digitalWrite(22, 0)
+                sleep(timeLen)
+                wiringpi.digitalWrite(5, 0)
+                sleep(timeLen)
+                wiringpi.digitalWrite(6, 0)
+                print("LOW")
+                sleep(timeLen)
+            except KeyboardInterrupt:
+                run = cycles
+                camera.stop_recording()
+                camera.stop_preview()
+                wiringpi.digitalWrite(17, 0)
+                wiringpi.digitalWrite(27, 0)
+                wiringpi.digitalWrite(22, 0)
+                wiringpi.digitalWrite(5, 0)
+                wiringpi.digitalWrite(6, 0)
+                sleep(1)
+                print("User ended premature")
+
+        camera.stop_recording()
+        camera.stop_preview()
+        return
 
 if __name__ == "__main__":
     automatic()
